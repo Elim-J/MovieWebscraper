12390b547083afaab2aa417f17bc0f9feb2ffd4e
elim-j <01CodePathways11@gmail.com>
2023-05-11 21:40:44 -0500

    Get title elements, parse and strip title elements, save titles in list.

--- MovieScraper.py
+++ MovieScraper.py
@@ -1,14 +1,25 @@
 from requests_html import HTMLSession
 
 s = HTMLSession()
-url = 'https://au.rollingstone.com/100-greatest-movies-of-all-time/page/2/'
 
-r = s.get(url)
+def get_title_element(page):
+    movie_titles = []
+    url = f'https://au.rollingstone.com/100-greatest-movies-of-all-time/page/{page}/' 
+    r = s.get(url)
+    # Return list of movie titles       
+    list = r.html.find('h3.c-list__title')
+    return list
 
-list = r.html.find('h3.c-list__title')
+def parse_title_element(list):
+    for element in list:
+        movie_titles.append(element.text.strip())
+
+        
 movie_titles = []
-for element in list:
-    movie_titles.append(element.text)
+
+for i in range(9):
+    list = get_title_element(i)
+    parse_title_element(list)
 
 for title in movie_titles:
     print(title)
