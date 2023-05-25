d20fe495706e2f8d242c851db8fdc9545d9d9707
elim-j <01CodePathways11@gmail.com>
2023-05-25 15:41:37 -0500

    Create dictionary for ranks & assign ranks to movie titles.

--- MovieScraper.py
+++ MovieScraper.py
@@ -21,5 +21,33 @@ for i in range(9):
     list = get_title_element(i)
     parse_title_element(list)
 
-for title in movie_titles:
-    print(title)
+# for title in movie_titles:
+#     print(title)
+
+# Create a ranking system on most 'dangerous' based off words
+# names first and then rank so that you can search by name to assign rank
+grief_dict = {'Kill':'1', 'Revenge':'2', 'Blood':'3', 'Dead':'4', 'Blade':'5', 'War':'6', 'Gun':'7', 'Die':'8'}
+movie_rank = {}
+# prints keys
+# for x in grief_dict:
+#     print(x)
+#     print(grief_dict.get(x))
+
+# prints values
+# for x in grief_dict.values():
+#     print(x)
+
+# prints key and values
+# for x,y in grief_dict.items():
+#     print(x,y)
+for movie in movie_titles:
+    for x in grief_dict:
+        if(movie.find(x) != -1):
+            movie_rank.update({movie:grief_dict.get(x)})
+
+for x,y in movie_rank.items():
+    print(x,y)
+# loop through movie_titles and compare them to dictionary
+# if movie_titles are in map, insert into new map with their rank
+# str.find(sub_str) if -1 then it is not in 
+# I need to loop through names... and then see if they are present in movie names. Will take O(n) time...
